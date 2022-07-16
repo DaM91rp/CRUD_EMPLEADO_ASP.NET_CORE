1d9e14e95a56a45032f2683f7436d053c16e4882
DaM91rp <david_rioja_paz@outlook.es>
2022-07-16 02:37:20 -0500

    CRUD empleado con Asp.Net Core MVC

---
+++ CRUDCOREEF/Models/DBCRUDCOREEFContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUDCOREEF.Models
+{
+    public partial class DBCRUDCOREEFContext : DbContext
+    {
+        public DBCRUDCOREEFContext()
+        {
+        }
+
+        public DBCRUDCOREEFContext(DbContextOptions<DBCRUDCOREEFContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cargo> Cargos { get; set; } = null!;
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cargo>(entity =>
+            {
+                entity.HasKey(e => e.IdCargo)
+                    .HasName("PK__CARGO__6C98562525F9F1A5");
+
+                entity.ToTable("CARGO");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpleado)
+                    .HasName("PK__EMPLEADO__CE6D8B9E6A74E335");
+
+                entity.ToTable("EMPLEADO");
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NombreCompleto)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.oCargo)
+                    .WithMany(p => p.Empleados)
+                    .HasForeignKey(d => d.IdCargo)
+                    .HasConstraintName("FK_Cargo");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
